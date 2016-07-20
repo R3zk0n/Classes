2259accb8a59213516a4604ca3821ca9e6ebdad7
R3zk0n <rezkon93@gmail.com>
2016-07-20 09:23:29 -0400

    CarlH First program (Non Mod)

---
+++ 0x01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(void) // Int means its gonna have a return value which means return 0;
+//Void means your not sending arguments to the function so it can run full no argc
+{
+  printf("Hello Reddit\n");
+  return 0; //Return control back to os,
+}
